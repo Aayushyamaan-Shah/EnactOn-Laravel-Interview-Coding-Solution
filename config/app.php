80fb24b129dc9e269f8e5b969af1f031bbd1f63e
A5Playz <aayushs474@gmail.com>
2024-03-26 05:28:46 +0530

    Added aliases for PrizeController

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'PrizeController' => App\Http\Controllers\PrizesController::class,
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
