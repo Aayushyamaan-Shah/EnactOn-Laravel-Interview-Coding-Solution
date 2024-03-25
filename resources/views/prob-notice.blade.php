99e019488e2bb1e1c5fa22062a393f20c07605c6
A5Playz <aayushs474@gmail.com>
2024-03-26 05:28:09 +0530

    Fixed the rounding bug

--- resources/views/prob-notice.blade.php
+++ resources/views/prob-notice.blade.php
@@ -4,7 +4,7 @@
     use App\Models\Prize;
     $current_probability = floatval(Prize::sum('probability'));
     $remaining_probability = 100.0 - $current_probability;
-    $msg = "The sum of all prizes must be 100%. Currently its " . $current_probability . "%. You have to add ". $remaining_probability ."% to the prize.";
+    $msg = "The sum of all prizes must be 100%. Currently its " . $current_probability . "%. You have to add ". round($remaining_probability, 2) ."% to the prize.";
 @endphp
 @if( $current_probability < 100)
     <div class="alert alert-danger">
