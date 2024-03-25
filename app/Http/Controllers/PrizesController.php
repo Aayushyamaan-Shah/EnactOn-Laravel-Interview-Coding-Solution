636be1f1be018896a09ca5a305a949660801ea26
A5Playz <aayushs474@gmail.com>
2024-03-26 05:27:37 +0530

    Updated all the functionalities

--- app/Http/Controllers/PrizesController.php
+++ app/Http/Controllers/PrizesController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Prize;
-
 use Illuminate\Http\Request;
+use App\Models\DistributedPrize;
+
+use App\Models\DistributedRecords;
 use App\Http\Requests\PrizeRequest;
 use App\Http\Controllers\Controller;
+use App\Models\TrulyDistributedPrize;
 use Illuminate\Support\Facades\Redirect;
 
 
@@ -39,7 +42,6 @@ class PrizesController extends Controller
         $remainingPercentage = 100.0 - $usedPercentage;
 
         return view('prizes.create', [
-            "msg"=>$msg,
             "usedPercentage"=>$usedPercentage,
             "remainingPercentage"=>$remainingPercentage,
         ]);
@@ -58,7 +60,7 @@ class PrizesController extends Controller
         $parsedProbabilityVal = floatval($request->input('probability'));
         if($parsedProbabilityVal < 0){
             return Redirect::back()->withErrors(['msg' => 'The probability field must not be lesser than 0%']);
-        }if($parsedProbabilityVal > $probabilitySum){
+        }if($parsedProbabilityVal > (100 - $probabilitySum)){
             return Redirect::back()->withErrors(['msg' => 'The probability field must not be greater than '.(100.0 - $probabilitySum).'%']);
         }
 
@@ -94,9 +96,18 @@ class PrizesController extends Controller
     public function update(PrizeRequest $request, $id)
     {
         $prize = Prize::findOrFail($id);
+
+        $newProbability = floatval($request->input('probability'));
+
+        if($newProbability != $prize->probability){
+            $this::reset();
+        }
+
         $prize->title = $request->input('title');
-        $prize->probability = floatval($request->input('probability'));
+        $prize->probability = $newProbability;
         $prize->save();
+
+        // Reset only if the prize is updated
 
         return to_route('prizes.index');
     }
@@ -112,15 +123,29 @@ class PrizesController extends Controller
         $prize = Prize::findOrFail($id);
         $prize->delete();
 
+        $this::reset();
+
         return to_route('prizes.index');
     }
 
+    // public function simulateRandom(Request $request){
+    //     for ($i = 0; $i < $request->number_of_prizes ?? 10; $i++) {
+    //         Prize::nextPrizeRandom();
+    //     }
+    //     return to_route('prizes.index');
+    // }
 
     public function simulate(Request $request)
     {
 
 
-        for ($i = 0; $i < $request->number_of_prizes ?? 10; $i++) {
+        $prizeCount = $request->number_of_prizes;
+        $nextPrize = Prize::nextPrize();
+        if(is_null($nextPrize)){
+            Prize::distributePrizes($prizeCount);
+        }
+
+        for ($i = 0; $i < $prizeCount ?? 10; $i++) {
             Prize::nextPrize();
         }
 
@@ -130,6 +155,96 @@ class PrizesController extends Controller
     public function reset()
     {
         // TODO : Write logic here
+        DistributedPrize::truncate();
         return to_route('prizes.index');
     }
+
+    public static function getIdealProbabilityDetails(){
+        $prizes = Prize::all();
+
+        $arrayToReturn = [];
+
+        foreach($prizes as $prize){
+            $arrayToReturn[] = [
+                'label' => $prize->title,
+                'y' => $prize->probability,
+            ];
+        }
+
+        return $arrayToReturn;
+    }
+
+    public static function getActualProbabilityDetails(){
+        $prizes = Prize::all();
+        $arrayToReturn = [];
+
+        foreach($prizes as $prize){
+            $distributedPrizeCount = 0;
+
+            if(!is_null($prize->distributedPrizes->count)){
+                $distributedPrizeCount = $prize->distributedPrizes->count;
+            }
+
+            $arrayToReturn[] = [
+                'label' => $prize->title,
+                'y' => $distributedPrizeCount,
+            ];
+        }
+
+        return $arrayToReturn;
+    }
+
+    // public static function thyTrulyRandom(){
+    //     $theySlector = (float)rand(0,10000);
+    //     $prizes = Prize::all();
+
+    //     $percentTotal = 0;
+
+    //     foreach($prizes as $prize){
+
+    //         if($theySlector > $percentTotal && ($theySlector <= $percentTotal + ($prize->probability*100))){
+
+
+    //             $percentTotal += ($prize->probability*100);
+
+    //             $newPrizeToGive = TrulyDistributedPrize::where('prize_id', '=', $prize->id)->first();
+
+    //             // dd($newPrizeToGive);
+    //             if(is_null($newPrizeToGive)){
+
+    //                 $newPrizeToGive = new TrulyDistributedPrize;
+    //                 $newPrizeToGive->prize_id = $prize->id;
+    //                 $newPrizeToGive->count = 1;
+    //                 $newPrizeToGive->save();
+    //             }else{
+    //                 $newPrizeToGive->count += 1;
+    //                 $newPrizeToGive->update();
+    //             }
+
+    //             return $prize;
+    //         }
+    //     }
+
+    // }
+
+    // public static function getActualTrulyRandomProbabilityDetails(){
+    //     $prizes = Prize::all();
+    //     $arrayToReturn = [];
+
+    //     foreach($prizes as $prize){
+    //         $distributedPrizeCount = 0;
+
+    //         if(!is_null($prize->trulyDistributedPrizes->count)){
+    //             $distributedPrizeCount = $prize->distributedPrizes->count;
+    //         }
+
+    //         $arrayToReturn[] = [
+    //             'label' => $prize->title,
+    //             'y' => $distributedPrizeCount,
+    //         ];
+    //     }
+
+    //     return $arrayToReturn;
+    // }
+
 }
