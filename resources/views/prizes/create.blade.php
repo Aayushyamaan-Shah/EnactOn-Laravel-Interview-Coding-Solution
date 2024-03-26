01efce6526348c77d853e527fc944690a3236201
A5Playz <aayushs474@gmail.com>
2024-03-26 05:30:01 +0530

    Added condinal checks showing the errors and display of old values

--- resources/views/prizes/create.blade.php
+++ resources/views/prizes/create.blade.php
@@ -4,23 +4,22 @@
 
 @include('prob-notice')
 
-	@if($errors->any())
-		<div class="alert alert-danger">
-			@foreach ($errors->all() as $error)
-				{{ $error }} <br>
-			@endforeach
-		</div>
+    @if($errors->any())
+        @foreach ($errors->all() as $error)
+        <div class="alert alert-danger">
+            {{ $error }} <br>
+        </div>
+        @endforeach
 	@endif
-
 	{!! Form::open(['route' => 'prizes.store']) !!}
 
 		<div class="mb-3">
 			{{ Form::label('title', 'Title', ['class'=>'form-label']) }}
-			{{ Form::text('title', null, array('class' => 'form-control')) }}
+			{{ Form::text('title', old('title'), array('class' => 'form-control')) }}
 		</div>
 		<div class="mb-3">
 			{{ Form::label('probability', 'Probability', ['class'=>'form-label']) }}
-			{{ Form::number('probability', null, array('class' => 'form-control','min' => '0','max' => '100', 'placeholder' => '0 - 100','step' => '0.01')) }}
+			{{ Form::number('probability', old('probability'), array('class' => 'form-control','min' => '0','max' => $remainingPercentage, 'placeholder' => '0 - '.$remainingPercentage,'step' => '0.01')) }}
 		</div>
 
 
